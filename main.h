e287dbc1601057db5cbbb2e1b9ab61da9766e86c
OmarTl2005 <hiba41145@gmail.com>
2023-04-22 19:00:25 +0000

    main.h -> the header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _HEAD_H_
+#define _HEAD_H_
+
+/************MACROS************/
+
+#define BUFFER_SIZE_ 1024
+
+/************FUNCTIONS************/
+
+int _printf(const char *format, ...);
+
+#endif
